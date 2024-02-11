34902f5903f10e380ea4f3d80a803a240e98ba1e
Tanha Tul Tasnim <123966715+Tttasnim@users.noreply.github.com>
2024-02-11 14:51:02 +0600

    Add files via upload
    lab class1

---
+++ T.py
@@ -0,0 +1,32 @@
+print(" show everything we type")
+print( )
+zafrin = "zafrin is a bad girl"
+print(zafrin)
+print(type(zafrin))
+int = 1873445659
+print(type(int))
+if 5>2:
+    print( " 5 is smaller then 2")
+else:
+    print("not smaller")
+x = "hi"
+y = 'hello'
+print(x + ' ' + y)
+x =45
+x ='taha'
+print(x)
+v = 19209828250
+f = 'Ali'
+print(v, f)
+fruits =["apple", "banana"]
+x, y = fruits
+print(x +' ' + y )
+print(x)
+print(y)
+
+x = 'awasome'
+def myfun():
+    x = 'aa'
+    print('python is '+x)
+myfun()
+print('python is ' +x)
